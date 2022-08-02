9c29871ce98eac4a4955c99c7aae0d85ca81f3bb
Marcelo Henrique <marcelohcb@msn.com>
2022-08-02 11:59:49 -0300

    upg

--- src/components/expenses/index.js
+++ src/components/expenses/index.js
@@ -1,79 +1,74 @@
 import { useEffect, useState } from "react";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 import "./index.css";
 
 export const Expenses = () => {
+  const [display, setDisplay] = useState("none");  
+  function handleDisplay(){
+    if(display === "flex"){
+      setDisplay("none")
+    }
+    if(display === "none"){
+      setDisplay("flex")
+    }
+  }
 
-const [display, setDisplay] = useState("none")
-
-
-function handleDisplay(){
-  if(display==="none"){
-    setDisplay("flex")
-   }
- if(display==="flex"){
-  setDisplay("none")
- }
-
-}
   const fields = [
     {
-      valor: "40px",
+      value: "40px",
       text: "mon",
-      color: "hsl(10, 79%, 65%)",
-      amount: "$17.45"
+      amount: "$17.45",
+      display:` ${display}`
     },
     {
-      valor: "70px",
+      value: "70px",
       text: "tue",
-      color: "hsl(10, 79%, 65%)",
-      amount:"$34.91"
+      amount: "$34.91",
+      display:` ${display}`
     },
     {
-      valor: "120px",
+      value: "120px",
       text: "wed",
-      color: "hsl(186, 34%, 60%)",
-      amount: "$52.36"
-    },  
+      amount: "$52.36",
+      display:` ${display}`
+    },
     {
-      valor: "65px",
+      value: "65px",
       text: "thu",
-      color: "hsl(10, 79%, 65%)",
-      amount: "$31.07"
+      amount: "$31.07",
+      display:` ${display}`
     },
     {
-      valor: "50px",
+      value: "50px",
       text: "fri",
-      color: "hsl(10, 79%, 65%)",
-      amount: "$23.39"
+      amount: "$23.39",
+      display:` ${display}`
     },
     {
-      valor: "90px",
+      value: "90px",
       text: "sat",
-      color: "hsl(10, 79%, 65%)",
-      amount: "$43.28"
+      amount: "$43.28",
+      display:` ${display}`
     },
     {
-      valor: "60px",
+      value: "60px",
       text: "sun",
-      color: "hsl(10, 79%, 65%)",
-      amount: "$25.48"
+      amount: "$25.48",
+      display:` ${display}`
     },
   ];
 
-  return (   <>
- 
-    <main>      
-        <div  className="top">
+  return (
+    <>
+      <main>
+        <div className="top">
           <div className="top-left">
             <p>My balance</p>
             <h2>$921.48</h2>
           </div>
 
           <div className="top-right">
-            <div className="circle-black">
-
-            </div>
+            <div className="circle-black"></div>
           </div>
         </div>
 
@@ -81,64 +76,88 @@ function handleDisplay(){
           <h2> Spending - Last 7 days</h2>
 
           <div className="fields">
-            
-            {fields.map((field,index) => {
-            
+            {fields.map((field, index) => {
               return (
-                <>                  
-                 <div className="boxes" onMouseOver={handleDisplay}  key={index}>
-                  <Amount display={display}  >
-                 <h5>{field.amount} </h5> 
-                  </Amount>
-                   <Div key={index} height={field.valor}  color={field.color}> </Div>
-                 <p>{field.text}</p>    
+                <>
+                  <div className="boxes" key={index}>
+                    <Amount display={field.display}  key={index}>
+                      <h5>{field.amount} </h5>
+                    </Amount>
+                    <Div key={index} height={field.value} color={field.color}  onClick={handleDisplay}>
+                    <P >{field.text}</P>
+                    </Div>
                    
-                    </div>
+                  </div>
                 </>
-              )              
-             
+              );
             })}
           </div>
           <HR />
           <div className="bottom">
             <div>
-            <p>Total this month</p>
-            <h1>$478.33</h1>
+              <p>Total this month</p>
+              <h1>$478.33</h1>
             </div>
-            
+
             <div>
-            <span className="porcent">+2.4%</span>
-            <p>from last month</p>
-            </div>           
+              <span className="porcent">+2.4%</span>
+              <p>from last month</p>
+            </div>
           </div>
-        </div> 
-     
-    </main>
-    </> 
-  ); 
-}
+        </div>
+      </main>
+    </>
+  );
+};
 const Amount = styled("div")`
-display:${(props)=>props.display};
-justify-content:center;
-align-items:center;
-color:hsl(33, 100%, 98%);
-background-color:hsl(25, 47%, 15%);
-height:30px;
-width:3rem;  
-border-radius:5px;
-margin-bottom:5px;
-cursor: pointer;
+  display: ${(props)=> props.display};
+  justify-content: center;
+  align-items: center;
+  color: hsl(33, 100%, 98%);
+  background-color: hsl(25, 47%, 15%);
+  height: 30px;
+  width: 3rem;
+  border-radius: 5px;
+  margin: 40px 0;
+  position:absolute;  
+  cursor: pointer;
+`;
+const fill = keyframes`
+0% {
+height:0px;
+
+}
+100%{
+ height: ${(props) => props.height}; 
+}
 `
-const Div = styled("div")` 
-  background-color: ${(props)=>props.color};
-  height: ${(props)=>props.height};
-  width:2.5rem;  
-  border-radius:5px;
-  cursor: pointer; 
-  transition:ease-in-out all 5s;
+const Div = styled("div")`
+  display:flex;
+  bottom:28%;
+  position:absolute;
+  margin:50px;
+  background-color: hsl(10, 79%, 65%);
+  width: 2.5rem;
+  border-radius: 5px;
+  cursor: pointer;
+  height: ${(props) => props.height};
+  text-decoration: line-through;
+  transition: 0.7s;
+  transform: scale(1.05);
+  animation: ${fill} 1s 0.1s forwards ease-in-out;  
+
+  &:hover {
+    background-color: hsl(186, 34%, 60%);
+  }
+`;
+const P = styled("p")`
+position: absolute;
+bottom: -30px;
 `
 const HR = styled("hr")`
-  background-color:hsl(28, 10%, 53%);
+  background-color: hsl(28, 10%, 53%);
   opacity: 0.2;
-  margin:20px 0;
-`
+  margin-top: 110px;
+  margin-bottom:20px;
+
+`;
